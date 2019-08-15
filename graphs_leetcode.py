e4d6e99d6dfbeb689979440a3cb54d48162f2992
pangr <pangr@umich.edu>
2019-08-15 11:06:30 -0400

    longest consecutive sequence leetcode 128

--- graphs_leetcode.py
+++ graphs_leetcode.py
@@ -235,6 +235,65 @@ class Solution(object):
                     count += self.bfs_island(grid, visited, i, j)
         return count
 
+    ###################################################################################################################
+    ###################################################################################################################
+    ############################################ LONGEST CONSECUTIVE SEQUENCE 128 #####################################
+    ###################################################################################################################
+    ###################################################################################################################
+    def longestConsecutive_nonGraphSolution(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+
+        Task: find the maximum length of a subset of consecutive numbers
+        """
+        if nums == []: return 0
+
+        nums_set = set(nums)
+        max_len = 1
+
+        for num in nums_set:
+            if num-1 not in nums_set:
+                start = num
+                curr_len = 1
+
+                while start+1 in nums_set:
+                    start += 1
+                    curr_len += 1
+
+                max_len = max(curr_len, max_len)
+
+        return max(curr_len, max_len)
+
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+
+        Task: find the maximum length of a subset of consecutive numbers
+        """
+        if nums == []: return 0
+
+        nums_set = set(nums)
+        seen = set()
+        max_len = 0
+
+        for num in nums_set:
+            if num not in seen:
+                queue = [num]
+                curr_len = 0
+                while queue:
+                    curr = queue[0]
+                    queue.pop(0)
+                    seen.add(curr)
+                    curr_len += 1
+                    if curr+1 not in seen and curr+1 in nums_set:
+                        queue.append(curr+1)
+                    if curr-1 not in seen and curr-1 in nums_set:
+                        queue.append(curr-1)
+                max_len = max(curr_len, max_len)
+        return max_len
+
 if __name__ == "__main__":
     ###################################################################################################################
     ###################################################################################################################
@@ -281,3 +340,10 @@ if __name__ == "__main__":
     ###################################################################################################################
     print(s.numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
     # 1
+
+    ###################################################################################################################
+    ###################################################################################################################
+    ############################################ LONGEST CONSECUTIVE SEQUENCE 128 #####################################
+    ###################################################################################################################
+    ###################################################################################################################
+    print(s.longestConsecutive([100,4,200,1,3,2]))
