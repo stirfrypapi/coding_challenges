ec37559c4579f6076ee6ae82794fa655d27f80a8
pangr <pangr@umich.edu>
2019-08-18 12:33:13 -0400

    started on tree question max depth leetcode 104

--- node_class.py
+++ node_class.py
@@ -1,4 +1,20 @@
 class Node(object):
     def __init__(self, val, neighbors=[]):
+        # graph node
         self.val = val
         self.neighbors = neighbors
+
+class NodeBinaryTree(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# class BinaryTree(object):
+#     def __init__(self, list):
+#         node_list = []
+#         for l in list:
+#             n = NodeBinaryTree(l)
+#             node_list.append(n)
+#
+#         for i in range(0, len()):
