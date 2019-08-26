ee94ddb891fa99d7cede913ec0ed3757076f3556
pangr <pangr@umich.edu>
2019-08-26 19:14:52 -0400

    added constructor from  sorted list

--- trees_graphs/BinarySearchTree.py
+++ trees_graphs/BinarySearchTree.py
@@ -1,6 +1,18 @@
+from trees_graphs.Traversal import Traversal
+from trees_graphs.Node import NodeBinaryTree
+
 class BinarySearchTree(object):
     def __init__(self, list):
-        return
+        list.sort()
+        def createBST(list):
+            if not list:
+                return None
+            mid = int(len(list)/2)
+            root = NodeBinaryTree(list[mid])
+            root.left = createBST(list[:mid])
+            root.right = createBST(list[mid+1:])
+            return root
+        self.root = createBST(list)
 
     ###################################################################################################################
     ###################################################################################################################
@@ -11,13 +23,29 @@ class BinarySearchTree(object):
         """
         :type root: TreeNode
         :rtype: bool
+
+        BSTs' inorder traversal is always in increasing order
         """
         if root is None: return True
-        parents = [root]
-        while parents:
-            for n in parents:
-                if n.left is not None and n.left.val > n.val: return False
-                if n.right is not None and n.right.val < n.val: return False
-            children = [[n.left, n.right] for n in parents]
-            parents = [n for pair in children for n in pair if n]
+        t = Traversal()
+        l = t.inorder_traversal(root)
+        for n in range(0, len(l)-1):
+            if l[n] > l[n+1]:
+                return False
         return True
+
+if __name__ == '__main__':
+    ###################################################################################################################
+    ###################################################################################################################
+    ################################ CREATE BST #######################################################################
+    ###################################################################################################################
+    ###################################################################################################################
+    list = [1, 2, 3, 4, 5]
+    bst = BinarySearchTree(list)
+    t = Traversal()
+    t.traverse(bst.root, 'inorder')
+    ###################################################################################################################
+    ###################################################################################################################
+    ################################ IS VALID BST? 98 #################################################################
+    ###################################################################################################################
+    ###################################################################################################################
