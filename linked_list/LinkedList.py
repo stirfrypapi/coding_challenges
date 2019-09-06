7bb2ffffc757bf4d8676b4af6607edc4062949d4
pangr <pangr@umich.edu>
2019-09-06 12:15:39 -0400

    starting linked list problems

---
+++ linked_list/LinkedList.py
@@ -0,0 +1,22 @@
+from linked_list.ListNode import ListNode
+
+class LinkedList:
+    def __init__(self, list):
+        self.root = ListNode(list[0])
+        ptr = self.root
+        for i in range(1, len(list)):
+            n = ListNode(list[i])
+            ptr.next = n
+            ptr = n
+
+    def print_list(self):
+        print()
+        n = self.root
+        while n:
+            print(str(n.val) + ' ->', end=' ')
+            n = n.next
+        print()
+
+if __name__ == '__main__':
+    linked_list = LinkedList([1, 2, 3, 4, 5])
+    linked_list.print_list()
