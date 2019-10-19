0075550526011911f356f60429760cf15eb1749d
pangr <pangr@umich.edu>
2019-10-19 18:41:56 -0400

    finished expression tree, leaving word break for dp solution

--- coding_challenges/qualtrics_onsite.py
+++ coding_challenges/qualtrics_onsite.py
@@ -18,20 +18,36 @@ def evaluate_expression_tree(root):
         if root.expression == '':
             return root.val
         for child_Node in root.children:
-            if root.expression == '+':
-                return running_answer + evaluate_expression_tree(child_Node)
-            if root.expression == '-':
-                return evaluate_expression_tree(child_Node) - evaluate_expression_tree(root)
-            if root.expression == '*':
-                return evaluate_expression_tree(child_Node) * evaluate_expression_tree(root)
-            if root.expression == '/':
-                return evaluate_expression_tree(child_Node) / evaluate_expression_tree(root)
+            if child_Node.expression == '':
+                if root.expression == '+':
+                    running_answer += rec(child_Node, running_answer)
+                if root.expression == '*':
+                    running_answer *= rec(child_Node, running_answer)
+            if child_Node.expression != '':
+                if root.expression == '+':
+                    running_answer += rec(child_Node, 1)
+        return running_answer
 
-    return rec(root, 0)
+    if root.expression == '+':
+        return rec(root, 0)
+    elif root.expression == '*':
+        return rec(root, 1)
 
 def wordbreak(word, dictionary):
-    pass
+    # given a word and a list of words (dictionary) return true if the word
+    # can be formed by a subset of words in dictionary. false if not
 
+    def rec(word):
+        if word in dictionary:
+            return True
+        if word == "":
+            return False
+        if len(word) == 1 and word not in dictionary:
+            return False
+        for i in range(1, len(word)):
+            return rec(word[0:i]) and rec(word[i:])
+
+    return rec(word)
 
 if __name__ == "__main__":
     root = Node()
@@ -44,3 +60,36 @@ if __name__ == "__main__":
     five.val = 5
     root.children = [three, four, five]
     print(evaluate_expression_tree(root))
+    """
+    +
+    | \ \ 
+    3  4  5
+    """
+    multiply = Node()
+    multiply.expression = '*'
+    multiply_three = Node()
+    multiply_three.val = 3
+    multiply_two = Node()
+    multiply_two.val = 2
+    multiply.children = [multiply_three, multiply_two]
+    root.children.append(multiply)
+    print(evaluate_expression_tree(root)) # expected 18
+    """
+    +
+    | \ \  \ 
+    3  4  5 *
+           / \ 
+          3    2
+    """
+    print(evaluate_expression_tree(multiply)) # expected 6
+    add = Node()
+    add.expression = '+'
+    six = Node()
+    six.val = 6
+    multiply = Node()
+    multiply.expression = '*'
+    multiply.children = [multiply_two, multiply_three]
+    add.children = [six, multiply]
+    print(evaluate_expression_tree(add)) # expected 12
+
+    print(wordbreak('iamamazing', ['i', 'am', 'amazing']))
